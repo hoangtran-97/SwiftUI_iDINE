0732c674a1726120965d0f8362b0f0a8f71d8a78
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-11-24 16:26:04 +0200

    First Commit

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,21 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Hoang Tran on 11/24/19.
+//  Copyright © 2019 Hoang Tran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow()
+    }
+}
