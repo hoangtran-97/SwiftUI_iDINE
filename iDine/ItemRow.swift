3ebbdcff4baea80db1a983f3eebaeadb774b132b
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2019-11-25 10:20:27 +0200

    Order working

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -9,13 +9,43 @@
 import SwiftUI
 
 struct ItemRow: View {
+    var item: MenuItem
+    static let colors:[String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: ItemDetail(item: item)){
+            HStack{
+                Image(item.thumbnailImage)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text(String("$\(item.price)"))
+                }
+                Spacer()
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
+            }
+            .padding()
+        }
+        
     }
 }
 
 struct ItemRow_Previews: PreviewProvider {
     static var previews: some View {
-        ItemRow()
+        ItemRow(item: MenuItem.example)
     }
 }
